fbf0360a1586e4f550e1abeffbb8e3a5b5a428f7
Edamamesukai <88695691+Edamamesukai@users.noreply.github.com>
2023-04-20 23:14:49 +0900

    プロセカのリザルトの数字だけを切り抜いた

--- rythmgame_result_calculation.js
+++ rythmgame_result_calculation.js
@@ -1,19 +1,43 @@
-var imagewidth = 1;
-var imageheight = 2;
+const canvas_id_list = ["image_canvas", "perfectimage", "greatimage", "goodimage", "badimage", "missimage"];
 
 function imageinput(target){
     console.log("画像を受け取りました。");
+
     var image = new Image();
     var reader = new FileReader();
+
     reader.onload = function (e) {
-        document.getElementById("image_test").setAttribute("src", e.target.result);
         image.src = e.target.result;
         image.onload = function () {
             console.log("横幅：%d\n高さ：%d", image.naturalWidth, image.naturalHeight);
-            console.log(document.getElementById("imagesize"));
             document.getElementById("imagesize").textContent = `縦：${image.naturalWidth}px, 横：${image.naturalHeight}px`;
+
+            // 各リザルトを取得するためのオフセットを設定
+            var xOffset = 690;
+            var yOffset = 440;
+
+            for (var canvas_id of canvas_id_list) {
+                // メインのcanvasの設定
+                var canvas = document.getElementById(canvas_id);
+                var context = canvas.getContext("2d");
+
+                // drawImage(image, xOffset, yOffset, width, height)
+                if (canvas_id === "image_canvas"){
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    context.drawImage(image, 0, 0);
+                } else {
+                    // drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+                    canvas.width = 80;
+                    canvas.height = 35;
+                    context.drawImage(image, xOffset, yOffset, 80, 40, 0, 0, 80, 40);
+                    yOffset += 40;
+                }
+            }
         }
     }
+
+    // 画像を文字列に変換
     reader.readAsDataURL(target.files[0]);
     console.log(target.files[0]);
 }
